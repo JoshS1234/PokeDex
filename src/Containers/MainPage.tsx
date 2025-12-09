3e10a7815914af6292fb75d41ba14ab4e8b9fada
Josh Spence <62989021+JoshS1234@users.noreply.github.com>
2025-12-09 22:27:41 +0000

    Updated CSS to make it more responsive

--- src/Containers/MainPage.tsx
+++ src/Containers/MainPage.tsx
@@ -12,9 +12,14 @@ type MainPageProps = {
 
 const MainPage = ({ pokeData }: MainPageProps) => {
   const [currentIndex, setcurrentIndex] = useState<number>(0);
+  const [showRightSide, setShowRightSide] = useState<boolean>(false);
 
   const incrementPokemonId = (increment: number) => {
     setcurrentIndex((currentIndex + increment + 151) % 151);
+  };
+
+  const toggleSide = () => {
+    setShowRightSide(!showRightSide);
   };
 
   useEffect(() => {}, [currentIndex]);
@@ -22,11 +27,21 @@ const MainPage = ({ pokeData }: MainPageProps) => {
   return (
     <div className="mainPageContainer">
       <div className="contentContainer">
-        <div className="pokedexSide pokedexSide_left">
+        <div
+          className={`pokedexSide pokedexSide_left ${
+            showRightSide ? "hidden-mobile" : ""
+          }`}
+        >
           <div className="pokedexSideHeader pokedexSideHeader_left">
             <h4 className="headerButtons headerButtons_big">PokeDex</h4>
-            <h4 className="headerButtons headerButtons_small">blah</h4>
-            <h4 className="headerButtons headerButtons_small">blah</h4>
+            <button
+              className="headerButtons headerButtons_small toggleButton"
+              onClick={toggleSide}
+              aria-label="Toggle to right side"
+            >
+              →
+            </button>
+            <h4 className="headerButtons headerButtons_small"></h4>
           </div>
           <div className="pokemonContentContainer pokemonContentContainer_left">
             <PokemonTile
@@ -36,8 +51,22 @@ const MainPage = ({ pokeData }: MainPageProps) => {
           </div>
         </div>
 
-        <div className="pokedexSide pokedexSide_right">
-          <div className="pokedexSideHeader pokedexSideHeader_right"></div>
+        <div
+          className={`pokedexSide pokedexSide_right ${
+            !showRightSide ? "hidden-mobile" : ""
+          }`}
+        >
+          <div className="pokedexSideHeader pokedexSideHeader_right">
+            <button
+              className="headerButtons headerButtons_small toggleButton"
+              onClick={toggleSide}
+              aria-label="Toggle to left side"
+            >
+              ←
+            </button>
+            <h4 className="headerButtons headerButtons_small"></h4>
+            <h4 className="headerButtons headerButtons_small"></h4>
+          </div>
           <div className="pokemonContentContainer pokemonContentContainer_right">
             <NumberInterface setCurrentIndex={setcurrentIndex} />
             <Speakers />
